60350e5cb7c2c60586e560d85d1a5154e83f9554
Pavel Dovgaluk <pavel.dovgaluk@gmail.com>
2017-08-21 08:21:20 +0300

    Fixed digits drawing for frame mode

--- game/Graphics.cpp
+++ game/Graphics.cpp
@@ -255,14 +255,14 @@ void game_draw_digits(uint16_t num, int len, int x, int y, uint8_t color)
 #ifdef FRAME_BUFFER
         if (use_frame_buffer)
         {
-            for (int8_t dy = 0 ; dy < FONT_HEIGHT ; ++dy)
+            for (int8_t dy = 0 ; dy < DIGIT_HEIGHT ; ++dy)
             {
                 if (y + dy >= 0 && y + dy < HEIGHT)
                 {
                     uint8_t dd = pgm_read_byte_near(digits_data + d * DIGIT_HEIGHT + dy);
                     for (int b = 0 ; b < DIGIT_WIDTH ; ++b)
                     {
-                        if ((dd >> (FONT_WIDTH - 1 - b)) & 1)
+                        if ((dd >> (DIGIT_WIDTH - 1 - b)) & 1)
                         {
                             if (x + b >= 0 && x + b < WIDTH)
                                 frame[y + dy][x + b] = game_make_color(color);
